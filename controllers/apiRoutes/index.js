c03accd9a4ebfc23a24a832154e4e287e414aef6
codeDevLogan <logandevscode@gmail.com>
2023-02-09 12:22:12 -0700

    Saving work. Finished the index.js interior code.

--- controllers/apiRoutes/index.js
+++ controllers/apiRoutes/index.js
@@ -1,1 +1,14 @@
+// Importing Express Router.
 const router = require('express').Router();
+
+// Importing all sister files to pass through.
+const logRoutes = require('./logRoutes');
+const ticketRoutes = require('./ticketRoutes');
+const userRoutes = require('./userRoutes');
+
+// Using the api sub-routes to allow their fetches.
+router.use('/log', logRoutes);
+router.use('/ticket', ticketRoutes);
+router.use('/user', userRoutes);
+
+module.exports = router;
--- controllers/apiRoutes/userRoutes.js
+++ controllers/apiRoutes/userRoutes.js
@@ -1,1 +1,50 @@
 const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/login', async (req, res) => {
+  try {
+    // This expression tries to find an instance in the User table where the email is the same as the one the user tried to log in with.
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    // This expression validates the password the user tried to use to log in.
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    // This method confirms the user login once both parameters have been validated and updates the session's logged_in boolean to true.
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    // This method erases all session data thus flipping every setting to its default.
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
