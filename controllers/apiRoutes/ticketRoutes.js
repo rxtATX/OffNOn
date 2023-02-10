b701aaa48ac1283e4d7bf3fcfa2fb2b5e5ab7414
codeDevLogan <logandevscode@gmail.com>
2023-02-10 14:07:45 -0700

    Patched Issue #63.

--- controllers/apiRoutes/ticketRoutes.js
+++ controllers/apiRoutes/ticketRoutes.js
@@ -10,8 +10,7 @@ router.post('/', async (req, res) => {
             urgency: req.body.urgency
         })
 
-        res.status(200).json('Successfully Added Ticket.')
-        res.redirect('/Ticket/newTicket.id')
+        res.status(200).json(newTicket);
     } catch (err) {
         res.status(500).json('Server Error: Could not add Ticket.');
     }
