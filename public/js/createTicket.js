38164e330f3b956cbe7905251a8ad52e4d8b08a9
Etowww <etow.engineering@gmail.com>
2023-02-10 11:53:34 -0700

    Created the createTicket.js script which is responsible for creating a new ticket post call. When the user submits the new ticket form if their ticket creation was successful they will be redirected to the newly created ticket page. If the ticket creation is not successful they will given an error message.

---
+++ public/js/createTicket.js
@@ -0,0 +1,39 @@
+// Start of Code [Evan Towlerton]
+
+// Initiliaze the handleClickEvent function
+const handleClickEvent = async (event) => {
+    //Prevent the page from reloading
+    event.preventDefault();
+
+    // Setting variables to the user input data values from the front end form
+    const subject = document.querySelector("#subject-input").value.trim();
+    const description = document.querySelector("#description-input").value.trim();
+    const urgency = document.querySelector("#urgency-input").value.trim();
+
+    // Checks to make sure title / description / urgency 
+    if (title && description && urgency) {
+
+        // Make a fetch call to the API endpoint to create a new ticket
+        const response = await fetch("/api/ticket", {
+            method: 'POST',
+            body: JSON.stringify({ subject, description, urgency }),
+            headers: { 'Content-Type': "application/json" }
+        })
+    // Check if the response is successful, then get the data from the response
+    if (response.ok) {
+        const data = await response.json()
+        // Redirect the user to the newly created ticket page
+        document.location.replace(`/ticket/${data.id}`)
+    } else {
+        // If the response is not successful inform the user of the failure to create a ticket
+        throw new Error("Failed to create ticket");
+    }
+    };
+};
+
+// Attach an event listener to the form with the id = ("#create")
+// which listens for a click event and then calls the handleClickEvent function
+// when the form is submitted
+document
+    .querySelector("#create")
+    .addEventListener("click", handleClickEvent);
