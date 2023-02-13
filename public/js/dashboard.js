8964ac0b9ef80fe23353f0e1cd91ec896a146842
Christoph551 <christophersimmonds551@gmail.com>
2023-02-13 11:06:03 -0700

    Saving changes to pull from main in prep to style the dashboard.

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,24 +1,24 @@
-const Unclaimed = document.querySelector('#unclaimed') 
-.addEventListener('click', function(event) { 
-  console.info(`Button Clicked!}`);
- });
+const Unclaimed = document.querySelector('#unclaimed')
+  .addEventListener('click', function (event) {
+    console.info(`Button Clicked!}`);
+  });
 
-const Pending = document.querySelector('#pending') 
-.addEventListener('click', function(event) { 
-  console.info(`Button Clicked!}`);
- });
+const Pending = document.querySelector('#pending')
+  .addEventListener('click', function (event) {
+    console.info(`Button Clicked!}`);
+  });
 
-const Resolved = document.querySelector('#resolved') 
-.addEventListener('click', function(event) { 
-  console.info(`Button Clicked!}`);
- });
+const Resolved = document.querySelector('#resolved')
+  .addEventListener('click', function (event) {
+    console.info(`Button Clicked!}`);
+  });
 
-const Claimed = document.querySelector('#claimed') 
-.addEventListener('click', function(event) { 
-  console.info(`Button Clicked!}`);
- });
+const Claimed = document.querySelector('#claimed')
+  .addEventListener('click', function (event) {
+    console.info(`Button Clicked!}`);
+  });
 
-const All = document.querySelector('#all') 
-.addEventListener('click', function(event) { 
-  console.info(`Button Clicked!}`);
- });
+const All = document.querySelector('#all')
+  .addEventListener('click', function (event) {
+    console.info(`Button Clicked!}`);
+  });
