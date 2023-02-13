e776f7a2c77c621582be0125b4a943c58aacdcaa
codeDevLogan <logandevscode@gmail.com>
2023-02-13 11:57:52 -0700

    Server-Side API post route for Ticket added. Testing it now...

--- controllers/apiRoutes/logRoutes.js
+++ controllers/apiRoutes/logRoutes.js
@@ -1,3 +1,19 @@
 const router = require('express').Router();
 
+const { log } = require('../../models');
+
+router.post('/:id', async(req, res) => {
+    try {
+        req.body.ticket_id = req.params.id;
+        if(!req.body.id) {
+            res.status(404).json({ message: 'Please include a valid ticket id.' });
+        }
+        req.body.user_id = req.session.user_id;
+        const logData = await log.create(req.body);
+        res.status(200).json(logData);
+    } catch (err) {
+        res.status(500).json( { err, message: 'Server Error: Please try again later.' });
+    }
+});
+
 module.exports = router;
