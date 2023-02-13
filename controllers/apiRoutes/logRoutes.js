96e1d8ee46e296d7fdb26cafcf72aebbba82c51b
codeDevLogan <logandevscode@gmail.com>
2023-02-13 13:20:04 -0700

    Fixed logRoutes issues according to requests.

--- controllers/apiRoutes/logRoutes.js
+++ controllers/apiRoutes/logRoutes.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
 
-const { log } = require('../../models');
+const { Log } = require('../../models');
 
 router.post('/:id', async(req, res) => {
     try {
         req.body.ticket_id = req.params.id;
-        if(!req.body.id) {
+        if(!req.body.ticket_id) {
             res.status(404).json({ message: 'Please include a valid ticket id.' });
         }
         req.body.user_id = req.session.user_id;
-        const logData = await log.create(req.body);
+        const logData = await Log.create(req.body);
         res.status(200).json(logData);
     } catch (err) {
         res.status(500).json( { err, message: 'Server Error: Please try again later.' });
