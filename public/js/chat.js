b4f08887deffcfc32c51c3194635c13bed354d54
Etowww <etow.engineering@gmail.com>
2023-02-13 11:53:38 -0700

    NEW UPDATE

---
+++ public/js/chat.js
@@ -0,0 +1,12 @@
+// Start of Code 
+
+// Hidden Span Toggle Listener [Evan Towlerton]
+
+// Selects all elements with class of "hide-eye"
+const hideEyeElements = document.querySelectorAll('.hide-eye');
+
+// Define the toggleHidden function, which will be called in response to a click event
+// on the any elements with the class 'hide-eye'
+const toggleHidden = (event) => {
+    
+}
