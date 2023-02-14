d97f22131f35dd0d84384cd9550cf8c9088fed28
awalker2411 <120272622+awalker2411@users.noreply.github.com>
2023-02-14 13:32:26 -0500

    Update createTicket.js
    Asked Evan to copy and paste what he had and sent it to me

--- public/js/createTicket.js
+++ public/js/createTicket.js
@@ -1,22 +1,23 @@
 // Start of Code [Evan Towlerton]
+// Find the modal within the DOM with the class "hidden-modal-aw"
 const modal = document.querySelector(".hidden-modal-aw");
 // Initiliaze the handleClickEvent function
 const handleClickEvent = async (event) => {
     //Prevent the page from reloading
     event.preventDefault();
-
     // Setting variables to the user input data values from the front end form
     const subject = document.querySelector("#subject-input").value.trim();
     const description = document.querySelector("#description-input").value.trim();
     const urgency = document.querySelector("#urgency-input").value.trim();
-
-    // Checks to make sure title / description / urgency 
-    if (title && description && urgency) {
-
+    // Checks to make sure subject / description / urgency exist in the user input field
+    if (subject && description && urgency) {
         // Make a fetch call to the API endpoint to create a new ticket
         const response = await fetch("/api/ticket", {
             method: 'POST',
-            body: JSON.stringify({ subject, description, urgency }),
+            body: JSON.stringify({
+                ticket_title: subject,
+                ticket_text: description,
+                urgency: urgency }),
             headers: { 'Content-Type': "application/json" }
         })
     // Check if the response is successful, then get the data from the response
@@ -30,15 +31,13 @@ const handleClickEvent = async (event) => {
     }
     };
 };
-
 // Attach an event listener to the form with the id = ("#create")
 // which listens for a click event and then calls the handleClickEvent function
 // when the form is submitted
-
 document
     .querySelector("#create")
     .addEventListener("click", handleClickEvent);
-
+/////////////////////////////////////////////////////////////////////////////////////////////////
 // Initialize the toggleNewTicket function on button click event
 function toggleNewTicket() {
     // Make the newTicket modal visible to user by adding a new class which makes it visible
