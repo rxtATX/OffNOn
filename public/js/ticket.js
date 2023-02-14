d476e2509bbe6541c4c4bc4d498d8bdb64a261a0
Michaela <mijensine@gmail.com>
2023-02-14 13:12:18 -0700

    saving changes

--- public/js/ticket.js
+++ public/js/ticket.js
@@ -27,11 +27,12 @@
 
 
 
-
+//initialize the function for the button/chat element click event
 function chatlogModal () {
     modal.classList.toggle("show-modal")
 }
 
+//
 document.getElementById ("ticket-item").addEventListener ("click", chatlogModal);
 
 
